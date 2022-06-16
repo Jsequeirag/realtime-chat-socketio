35996e490322027feccf6b3f624b22b192805eb0
Jose Luis Sequeira Góngora <joseluissequeirag2@gmail.com>
2022-06-16 08:11:48 -0700

    function getUsersInroom return connected users

--- server/src/users.js
+++ server/src/users.js
@@ -3,18 +3,14 @@ const users = [];
 const addUser = ({ id, name, room }) => {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
-
   const existingUser = users.find(
     (user) => user.room === room && user.name === name
   );
 
   if (!name || !room) return { error: "Username and room are required." };
   if (existingUser) return { error: "Username is taken." };
-
   const user = { id, name, room };
-
   users.push(user);
-  console.log("agregar" + JSON.stringify(users[0]));
   return { user };
 };
 
@@ -23,11 +19,9 @@ const removeUser = (id) => {
   if (index !== -1) return users.splice(index, 1)[0];
 };
 const getUser = (id) => {
-  console.log("param" + id);
-  console.log("enviar" + JSON.stringify(users[0]));
   return users.find((user) => user.id === id);
 };
 const getUsersInRoom = (room) => {
-  users.filter((user) => user.room === room);
+  return users.filter((user) => user.room === room);
 };
 module.exports = { addUser, removeUser, getUser, getUsersInRoom };
